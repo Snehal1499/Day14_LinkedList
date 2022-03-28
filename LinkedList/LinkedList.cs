10cdf8d9301c73542f66ac4747c69ccac6e25114
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-28 21:46:53 +0530

    Added UC3 LinkedList Program

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -44,6 +44,89 @@ namespace LinkedList
                 }
             }
         }
+        internal Node InsertAtParticulatPosition(int pos, int data)
+        {
+            if (pos < 1)
+                Console.WriteLine("invalid Position");
+            if (pos == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (pos-- != null)
+                {
+                    if (pos == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (pos != 1)
+                {
+                    Console.WriteLine("Position out of range");
+                }
+            }
+            return head;
+        }
+        internal Node PoP_First_element()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        internal Node PoP_Last_element()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+        internal void PoP_Paricular_element(int pos)
+        {
+            Node temp = head;
+            for (int i = 2; i < pos; i++)
+            {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+        internal int Search(int value)
+        {
+            Node node = this.head;
+            int count = 1;
+            while (node != null)
+            {
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+
+            }
+            return count;
+
+        }
     }
 }
 
